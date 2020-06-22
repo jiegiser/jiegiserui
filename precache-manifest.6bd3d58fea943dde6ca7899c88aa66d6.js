830db376e34ae17e5ab52bf4ed40f19803235e87
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-22 07:09:42 +0000

    Deploy jiegiser/jiegiserui to github.com/jiegiser/jiegiserui.git:gh-pages

---
+++ precache-manifest.6bd3d58fea943dde6ca7899c88aa66d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993e7b96cdf1235cb76a98c5b226b329",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.dfe88bea4820f4a33b88.bundle.js"
+  },
+  {
+    "url": "runtime~main.dfe88bea4820f4a33b88.bundle.js"
+  },
+  {
+    "revision": "176d9e9e0819f0261e62",
+    "url": "static/css/main.94f8e50e.chunk.css"
+  },
+  {
+    "revision": "d1da5dd68d8a1e793e9a",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.dfe88bea4820f4a33b88.bundle.js"
+  },
+  {
+    "url": "vendors~main.dfe88bea4820f4a33b88.bundle.js.LICENSE.txt"
+  }
+]);
