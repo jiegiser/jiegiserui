b50e6397a612ae1dab99b10e0f91f76b989a8ca5
jiegiser <cjieouba@outlook.com>
2020-06-22 15:00:09 +0800

    Fix: 修改文档页面

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -6,6 +6,7 @@ storiesOf('Welcome page', module)
     return (
       <>
         <h1>欢迎来到 jiegiserui 组件库</h1>
+        <h2>基于 react 的组件库</h2>
         <h3>安装试试</h3>
         <code>
           npm install jiegiserui --save
